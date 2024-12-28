a3be9a258d6c598fde92518f9d67f939d90a41e4
Luka Stajković <luka.stajkovic@gmail.com>
2024-12-28 17:58:46 +0100

    Fix: Reseni problemi sa autentifikacijom

--- Domaci1/fitness-web-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ Domaci1/fitness-web-app/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
@@ -13,26 +11,34 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(Request $request)
     {
-        $request->authenticate();
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
-        $request->session()->regenerate();
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid login credentials'], 401);
+        }
 
-        return response()->noContent();
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 200);
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request)
     {
-        Auth::guard('web')->logout();
+        $request->user()->tokens()->delete();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return response()->json(['message' => 'Logout successful'], 200);
     }
 }
